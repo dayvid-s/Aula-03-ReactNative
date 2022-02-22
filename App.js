eae2da12a6c3f201d46e0370e2429bc64ef448c1
Dayvid Moreno Dos Santos <dayvid7santos2002@outlook.com>
2022-02-22 16:35:28 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+//        ESSA AULA FOI SOBRE COMPONENTES DE CLASSE E DE FUNÇÃO.
+//
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class App1 extends Component {
+  render() {
+    return (
+      <View>
+        <Text>CFB CURSOsS</Text>
+      </View>
+    );
+  }
+}
+
+//esse aqui é o componente de função
+/*
+export default function App1(){
+  return (
+    <View>
+      <Text>CFB CURSOS</Text>
+    </View>
+  );
+}
+*/
